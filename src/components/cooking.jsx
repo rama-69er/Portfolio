99369f895290f7ce1270d08c95460303f03c6bd3
Ramanand Dubey <167943838+rama-69er@users.noreply.github.com>
2025-02-20 07:41:39 +0530

    Update cooking.jsx

--- src/components/cooking.jsx
+++ src/components/cooking.jsx
@@ -4,12 +4,12 @@ import Cards from './cards'
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 const Hobbies = [
-  { id: 1, name: "Biryani", img: "/images/cooking.jpg", type: "Outdoor", rateIcon: faStar, ratePoint: 4 },
-  { id: 2, name: "Kabab", img: "/images/cooking.jpg", type: "Outdoor", rateIcon: faStar, ratePoint: 4 },
-  { id: 3, name: "Matar Paneer", img: "/images/cooking.jpg", type: "Indoor", rateIcon: faStar, ratePoint: 4 },
-  { id: 4, name: "Chukauni", img: "/images/cooking.jpg", type: "Outdoor", rateIcon: faStar, ratePoint: 4 },
-  { id: 5, name: "Shimla Mirch Payaz Masala", img: "/images/cooking.jpg", type: "Outdoor", rateIcon: faStar, ratePoint: 3 },
-  { id: 6, name: "Chicken Curry", img: "/images/cooking.jpg", type: "Outdoor", rateIcon: faStar, ratePoint: 2 },
+  { id: 1, name: "Biryani", img: "/images/cooking.jpg", type: "Veg", rateIcon: faStar, ratePoint: 3 },
+  { id: 2, name: "Kabab", img: "/images/cooking.jpg", type: "Veg", rateIcon: faStar, ratePoint: 4 },
+  { id: 3, name: "Matar Paneer", img: "/images/cooking.jpg", type: "Veg", rateIcon: faStar, ratePoint: 4 },
+  { id: 4, name: "Shimla Mirch Payaz Masala", img: "/images/cooking.jpg", type: "Veg", rateIcon: faStar, ratePoint: 4 },
+  { id: 5, name: "Fish Curry", img: "/images/cooking.jpg", type: "Non-Veg", rateIcon: faStar, ratePoint: 4 },
+  { id: 6, name: "Chicken Curry", img: "/images/cooking.jpg", type: "Non-Veg", rateIcon: faStar, ratePoint: 5 },
 ]
 
 function Cooking() {
