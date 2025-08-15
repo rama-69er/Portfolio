7406207bd81726e13d84236ed1e3363322470b06
Ramanand Dubey <rdx-69er@users.noreply.github.com>
2025-08-15 12:29:27 +0530

    changed...

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../css/homepage.css";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
@@ -11,69 +11,67 @@ import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 function Header() {
     return (
         <>
-            <section className="homePage">
-                <div className="homepage-image">
-                    <div className="homeTab">
-                        <div className="container">
-                            <div className="homepage-tab">
-                                <div className="row">
-                                    <div className="col-lg-6">
-                                        <div className="left-tab">
-                                            <div className="row">
-                                                <div className="col-md-4">
-                                                    <img
-                                                        id="profile-image"
-                                                        src="/images/dp.webp"
-                                                        alt="ramanand"
-                                                    />
+            <section className="homePage py-3 py-md-5">
+                <div className="container">
+                    <div className="homeTab p-4 rounded-4">
+                        <div className="row align-items-center justify-content-center">
+                            <div className="col-lg-6">
+                                <div className="left-tab">
+                                    <div className="row align-items-center">
+                                        <div className="col-md-6">
+                                            <img
+                                                src="/images/dp.webp"
+                                                alt="Best Portfolio"
+                                                className="profile-image img-fluid rounded-4 border-2 h-100 w-100 mb-3 mb-lg-0"
+                                            />
+                                        </div>
+                                        <div className="col-md-6">
+                                            <div className="title text-center mb-3 mb-md-0 text-white">
+                                                <div className="mb-3">
+                                                    <h1>Ramanand Dubey</h1>
+                                                    <h5>Software Developer</h5>
                                                 </div>
-                                                <div className="col-md-8">
-                                                    <div className="title">
-                                                        <h1>Ramanand Dubey</h1>
-                                                        <h5>Software Developer</h5>
-                                                        <ul>
-                                                            <li>
-                                                                <Link target="_blank" rel="noreferrer" to="https://www.facebook.com/ramanand.dubey.988/"  className="theme-btn facebook">
-                                                                    <FontAwesomeIcon icon={faFacebook} />
-                                                                </Link>
-                                                            </li>
-                                                            <li>
-                                                                <Link target="_blank" rel="noreferrer" to="https://www.instagram.com/rd.x_69er/" className="theme-btn instagram">
-                                                                    <FontAwesomeIcon icon={faInstagram} />
-                                                                </Link>
-                                                            </li>
-                                                            <li>
-                                                                <Link target="_blank" rel="noreferrer" to="https://www.linkedin.com/in/ramanand-dubey-985b9025a/" className="theme-btn linkedIn">
-                                                                    <FontAwesomeIcon icon={faLinkedin} />
-                                                                </Link>
-                                                            </li>
-                                                        </ul>
-                                                    </div>
-                                                </div>
+                                                <ul className="list-unstyled d-flex align-items-center justify-content-evenly gap-3">
+                                                    <li>
+                                                        <Link target="_blank" rel="noreferrer" to="https://www.facebook.com/ramanand.dubey.988/" className="theme-btn facebook">
+                                                            <FontAwesomeIcon icon={faFacebook} />
+                                                        </Link>
+                                                    </li>
+                                                    <li>
+                                                        <Link target="_blank" rel="noreferrer" to="https://www.instagram.com/rd.x_69er/" className="theme-btn instagram">
+                                                            <FontAwesomeIcon icon={faInstagram} />
+                                                        </Link>
+                                                    </li>
+                                                    <li>
+                                                        <Link target="_blank" rel="noreferrer" to="https://www.linkedin.com/in/ramanand-dubey-985b9025a/" className="theme-btn linkedIn">
+                                                            <FontAwesomeIcon icon={faLinkedin} />
+                                                        </Link>
+                                                    </li>
+                                                </ul>
                                             </div>
                                         </div>
                                     </div>
-                                    <div className="col-lg-6">
-                                        <div className="right-tab">
-                                            <div className="row">
-                                                <div className="col-lg-6 col-md-6 col-sm-12">
-                                                    <h5>Email</h5>
-                                                    <p>ramanand.dubey@yahoo.com</p>
-                                                </div>
-                                                <div className="col-lg-6 col-md-6 col-sm-12">
-                                                    <h5>Birth</h5>
-                                                    <p>21 June, 2001</p>
-                                                </div>
-                                                <div className="col-lg-6 col-md-6 col-sm-12">
-                                                    <h5>Phone</h5>
-                                                    <p>(+91) 8005633372</p>
-                                                    <p>(+91) 8302491984</p>
-                                                </div>
-                                                <div className="col-lg-6 col-md-6 col-sm-12">
-                                                    <h5>Address</h5>
-                                                    <p>Kaptanganj, Azamgarh, U.P.</p>
-                                                </div>
-                                            </div>
+                                </div>
+                            </div>
+                            <div className="col-lg-6">
+                                <div className="right-tab text-white">
+                                    <div className="row align-items-center justify-content-center">
+                                        <div className="col-sm-6">
+                                            <h5>Email</h5>
+                                            <p>ramanand.dubey@yahoo.com</p>
+                                        </div>
+                                        <div className="col-sm-6">
+                                            <h5>Birth</h5>
+                                            <p>21 June, 2001</p>
+                                        </div>
+                                        <div className="col-sm-6">
+                                            <h5>Phone</h5>
+                                            <p className="mb-0">(+91) 8005633372</p>
+                                            <p>(+91) 8302491984</p>
+                                        </div>
+                                        <div className="col-sm-6">
+                                            <h5>Address</h5>
+                                            <p className="mb-0">Kaptanganj, Azamgarh, U.P.</p>
                                         </div>
                                     </div>
                                 </div>
@@ -82,8 +80,7 @@ function Header() {
                     </div>
                 </div>
             </section>
-        </>
-    )
+        </>)
 }
 
 export default Header;
--- src/components/music.jsx
+++ src/components/music.jsx
@@ -4,23 +4,23 @@ function Music() {
     return (
         <div className='cards-page'>
             <div className="row">
-                <div className="col-md-6 mt-4">
-                    <iframe style={{ borderRadius: "12px" }} src="https://open.spotify.com/embed/playlist/3rW2zfWIx59iMS0BS4OJSO?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                <div className="col-md-6">
+                    <iframe className='mb-3 rounded h-100' title='playlist1' src="https://open.spotify.com/embed/playlist/3rW2zfWIx59iMS0BS4OJSO?utm_source=generator" width="100%" height="152" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                 </div>
-                <div className="col-md-6 mt-4">
-                    <iframe style={{ borderRadius: "12px" }} src="https://open.spotify.com/embed/playlist/5SS4gw1XqCWWBgpOA13k7m?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                <div className="col-md-6">
+                    <iframe className='mb-3 rounded h-100' title='playlist2' src="https://open.spotify.com/embed/playlist/5SS4gw1XqCWWBgpOA13k7m?utm_source=generator" width="100%" height="152" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                 </div>
-                <div className="col-md-6 mt-4">
-                    <iframe style={{ borderRadius: "12px" }} src="https://open.spotify.com/embed/playlist/0JN9k6LYnMqN5GuR29nzG5?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                <div className="col-md-6">
+                    <iframe className='mb-3 rounded h-100' title='playlist3' src="https://open.spotify.com/embed/playlist/0JN9k6LYnMqN5GuR29nzG5?utm_source=generator" width="100%" height="152" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                 </div>
-                <div className="col-md-6 mt-4">
-                    <iframe style={{ borderRadius: "12px" }} src="https://open.spotify.com/embed/playlist/14Xxz3sCe85a9ofYKlpShv?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                <div className="col-md-6">
+                    <iframe className='mb-3 rounded h-100' title='playlist4' src="https://open.spotify.com/embed/playlist/14Xxz3sCe85a9ofYKlpShv?utm_source=generator" width="100%" height="152" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                 </div>
-                <div className="col-md-6 mt-4">
-                    <iframe style={{ borderRadius: "12px" }} src="https://open.spotify.com/embed/playlist/15xeSkxbwQ0WoIuaVX07z9?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                <div className="col-md-6">
+                    <iframe className='mb-3 rounded h-100' title='playlist5' src="https://open.spotify.com/embed/playlist/15xeSkxbwQ0WoIuaVX07z9?utm_source=generator" width="100%" height="152" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                 </div>
-                <div className="col-md-6 mt-4">
-                    <iframe style={{ borderRadius: "12px" }} src="https://open.spotify.com/embed/playlist/4A3GvVR6i8AqUFTvEIJAjj?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                <div className="col-md-6">
+                    <iframe className='mb-3 rounded h-100' title='playlist6' src="https://open.spotify.com/embed/playlist/4A3GvVR6i8AqUFTvEIJAjj?utm_source=generator" width="100%" height="152" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                 </div>
 
             </div>
